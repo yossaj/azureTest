b1fd33298a12a94a8afe86b6931e9ca4239e98d6
yossaj <yossaj@icloud.com>
2019-08-12 19:59:47 +0100

    inital commit

---
+++ precache-manifest.ed3917af8ecc92d9c40d9226ac9f98c9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45a94e1969621b06a4d593b9afa7d198",
+    "url": "/index.html"
+  },
+  {
+    "revision": "70b79defb137bda2b91e",
+    "url": "/static/css/main.cfda8877.chunk.css"
+  },
+  {
+    "revision": "9ce96f0eb542d7d5e3db",
+    "url": "/static/js/2.bf10928b.chunk.js"
+  },
+  {
+    "revision": "70b79defb137bda2b91e",
+    "url": "/static/js/main.da31848a.chunk.js"
+  },
+  {
+    "revision": "e1649a1332b8d54cfecd",
+    "url": "/static/js/runtime~main.167b8651.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
